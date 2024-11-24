cb5132a67ff16d242d898e0f2462754eabf19c5d
Maria <pavla0806@gmail.com>
2024-11-24 16:21:56 +0300

    Внесенные изменения

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Message from './components/Message';
 
 
 function App() {
+  const date = new Date();
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const today = new Intl.DateTimeFormat('en-US', options).format(date);
   return (
     <div className="App">
-      <h1>Homework #1 </h1>
-      <Message today='formattedDate' />
-    </div>
+      <h1 className="header">Homework #1</h1>
+      <h2>Сегодня: <span><Message
+        text={today} /></span>
+      </h2>
+    </div >
   );
 }
 
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,17 +1,10 @@
 import '..'
-import MessageStyle from '../MessageStyle.css';
+import messageStyle from './messageStyle.css';
 
-function Message({ }) {
-    const date = new Date();
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const today = new Intl.DateTimeFormat('en-US', options).format(date);
-
-    return (
-        <div className='message'>
-
-            <h2>Сегодня <span>{today}</span></h2>
-        </div>
-    )
-}
+function Message({ text }) {
+    return <div>
+        <p className="Message"><span>{text}</span></p>
+    </div>;
+};
 
 export default Message;
